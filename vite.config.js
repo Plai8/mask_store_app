55d945143cf1bc10daf935863214b667b8de5090
Plai8 <a0910930190@gmail.com>
2025-05-21 10:11:30 +0800

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:"/mask-app/",
+  base:"/mask_store_app/",
   plugins: [
     vue(),
     vueDevTools(),
